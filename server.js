fce87cc4f6503fc14dfbc1b757931c321dd2dfa3
Ollie Parsley <ollie@ollieparsley.com>
2018-12-19 22:37:13 +0000

    Order and combine keywords to compare and generate simhash

--- server.js
+++ server.js
@@ -56,66 +56,78 @@ apiRouter.post('/article', (req, res) => {
 
     // Metadata
     htmlMetadata.loadFromString(article.html).then(function(metadata){
-      data.meta = metadata;
+      try {
+        data.meta = metadata;
 
-      // Sanitized content
-      data.content = {};
-      data.content.text = sanitizeHtml(article.content, {
-        allowedTags: [ 'h2', 'h3', 'h4', 'h5', 'h6', 'blockquote', 'p', 'ul', 'ol',
-          'nl', 'li', 'b', 'i', 'strong', 'em', 'strike', 'code', 'hr', 'br' ]
-      }).replace(/<\/?[^>]+(>|$)/g, "").replace(/\s+/g,' ').trim();
-      data.content.simhash = simhash.hash(data.content.text);
+        // Sanitized content
+        data.content = {};
+        data.content.text = sanitizeHtml(article.content, {
+          allowedTags: [ 'h2', 'h3', 'h4', 'h5', 'h6', 'blockquote', 'p', 'ul', 'ol',
+            'nl', 'li', 'b', 'i', 'strong', 'em', 'strike', 'code', 'hr', 'br' ]
+        }).replace(/<\/?[^>]+(>|$)/g, "").replace(/\s+/g,' ').trim();
+        data.content.simhash = simhash.hash(data.content.text);
 
-      // Quotes
-      quotes = [];
-      data.content.text.match(/"([^"]+)"/g).forEach((item, i) => {
-        if (i === 0) {
-          return;
-        }
-        if (item.toString().length < 40) {
-          return;
-        }
-        if (item.toString().length > 140) {
-          return;
-        }
-        var text = item.substr(1, item.length -2);
-        quotes.push({
-          text: text,
-          simhash: simhash.hash(text)
-        });
-      });
-      data.quotes = {short:quotes};
-
-      // Run LDA to get terms for 2 topics (5 terms each).
-      data.nlp = {};
-      var ldaResult = lda([data.content.text], 3, 5);
-      data.nlp.lda = ldaResult;
-
-      // Keyword extractor
-      data.nlp.keywords = keywordExtractor.extract(data.content.text, {
-        language: "english",
-        remove_digits: true,
-        return_changed_case:true,
-        remove_duplicates: true,
-        return_chained_words: true,
-        remove_max_ngrams: false
-      });
-
-      // Find the most relevant keywords in the headline, sorted by number of appearances in the body text
-      data.nlp.top_keywords = [];
-      var topKeywords = headlineParser.findKeywords(data.title.text + ' ' + data.meta.general.description, data.content.text, 10);
-      topKeywords.forEach((topKeyword) => {
-        data.nlp.top_keywords.push({
-          text: pluralize.singular(topKeyword),
-          synonyms: {
-            nouns: synonyms(topKeyword, 'n'),
-            verbs: synonyms(topKeyword, 'v')
+        // Quotes
+        quotes = [];
+        data.content.text.match(/"([^"]+)"/g).forEach((item, i) => {
+          if (i === 0) {
+            return;
           }
+          if (item.toString().length < 40) {
+            return;
+          }
+          if (item.toString().length > 140) {
+            return;
+          }
+          var text = item.substr(1, item.length -2);
+          quotes.push({
+            text: text,
+            simhash: simhash.hash(text)
+          });
         });
-      });
-      console.log("Article fetched: " + articleUrl + ": " , data);
-      res.status(200).json({article: data});
-  });
+        data.quotes = {short:quotes};
+
+        // Run LDA to get terms for 2 topics (5 terms each).
+        data.nlp = {};
+        var ldaResult = lda([data.content.text], 3, 5);
+        data.nlp.lda = ldaResult;
+
+        // Keyword extractor
+        data.nlp.keywords = keywordExtractor.extract(data.content.text, {
+          language: "english",
+          remove_digits: true,
+          return_changed_case:true,
+          remove_duplicates: true,
+          return_chained_words: true,
+          remove_max_ngrams: false
+        });
+
+        // Find the most relevant keywords in the headline, sorted by number of appearances in the body text
+        data.nlp.top_keywords = {
+          keywords: []
+        };
+        var topKeywords = headlineParser.findKeywords(data.title.text + ' ' + data.meta.general.description, data.content.text, 10);
+        keywordList = [];
+        topKeywords.forEach((topKeyword) => {
+          keywordList.push(topKeyword);
+          data.nlp.top_keywords.keywords.push({
+            text: pluralize.singular(topKeyword),
+            synonyms: {
+              nouns: synonyms(topKeyword, 'n'),
+              verbs: synonyms(topKeyword, 'v')
+            }
+          });
+        });
+        data.nlp.top_keywords.simhash_raw = keywordList.sort().join(' ');
+        data.nlp.top_keywords.simhash = simhash.hash(data.nlp.top_keywords.simhash_raw);
+        console.log("Article fetched: " + articleUrl + ": " , data);
+        res.status(200).json({article: data});
+        
+      } catch(err) {
+        console.error("Response error: " + articleUrl + ": " , err);
+        res.status(500).json({message: 'Something went wrong creating the response', exception: err.message});
+      }
+    });
 
     // Close article to clean up jsdom and prevent leaks
     setTimeout(function(){
